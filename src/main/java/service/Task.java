f386f8dd7dd9e619b34b21057dfa682ab7e40a24
Oreneile Sejeso <sejesoo@gmail.com>
2025-03-15 00:54:34 +0200

    Implemented the TaskManager class

--- src/main/java/service/Task.java
+++ src/main/java/service/Task.java
@@ -1,5 +1,26 @@
 package service;
 
-public class Task {
 
+//Task class to store details of a task
+public class Task {
+	
+	String title;
+	boolean isCompleted;
+
+	public Task(String title) {
+		this.title = title;
+		this.isCompleted = false;
+		
+	}
+	
+	public void markCompleted() {
+		this.isCompleted = true;
+	}
+
+	@Override
+	public String toString() {
+		return (isCompleted ? "[ v ]": "[ ]") + title;
+	}
+	
+	
 }
--- src/main/java/service/TaskManager.java
+++ src/main/java/service/TaskManager.java
@@ -1,5 +1,58 @@
 package service;
 
-public class TaskManager {
+import java.util.LinkedList;
 
+//Task Manager class using LinkedList
+public class TaskManager {
+	
+	private LinkedList<Task> taskList;
+	
+	public TaskManager() {
+		this.taskList = new LinkedList<Task>();
+	}
+	
+	// Add a new task
+	public void addTask(String title) {
+		
+		taskList.add(new Task(title));
+		System.out.println("Task added: " + title);
+		
+	}
+	
+	// Remove a task by index
+	public void removeTask(int index) {
+		
+		if(index >=0 && index < taskList.size()) {
+			Task removedTask = taskList.remove(index);
+			System.out.println("Removed Task: " + removedTask.title);
+		}else {
+			System.out.println("Invalid task index");
+		}
+		
+	}
+	
+	// Mark a task as completed
+	public void compeleteTask(int index) {
+		
+		if(index >=0 && index < taskList.size()) {
+			taskList.get(index).markCompleted();;
+			System.out.println("Task marked as completed: " + taskList.get(index).title);
+		}else {
+			System.out.println("Invalid task index");
+		}
+		
+	}
+	
+	// Display all tasks
+	public void displayTasks() {
+		if(taskList.isEmpty()) {
+			System.out.println("No tasks available");
+		}else {
+			System.out.println("Task list: ");
+			for(int i=0; i<=taskList.size();i++) {
+				System.out.println(i + "." + taskList.get(i));
+			}
+		}
+		
+	}
 }
